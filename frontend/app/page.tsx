318cb29e48981018db228c0d062780d11895843a
nick siscoe <nick.siscoe@gmail.com>
2023-09-23 15:38:58 -0500

    connection header fe

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { GameContextType, useGameContext } from "./contexts/GameContext";
 import Welcome from "./components/Welcome";
 import ChatFeed from "./components/ChatFeed";
@@ -24,8 +24,50 @@ function Game({ state }: { state: GameState }) {
 export default function Home() {
   const gameContext = useGameContext();
 
+  const [showConnectionHeader, setShowConnectionHeader] =
+    useState<boolean>(true);
+  useEffect(() => {
+    setShowConnectionHeader(true);
+
+    if (!!gameContext.connected) {
+      setTimeout(() => {
+        setShowConnectionHeader(false);
+      }, 2500);
+    }
+  }, [gameContext.connected]);
+  const renderConnectionHeader = () => {
+    if (!showConnectionHeader) return null;
+
+    switch (gameContext.connected) {
+      case undefined: {
+        return (
+          <div className={styles.connectionHeader}>
+            <p>Connecting...</p>
+          </div>
+        );
+      }
+      case true: {
+        return (
+          <div className={`${styles.connectionHeader} ${styles.connected}`}>
+            <p>Connected.</p>
+          </div>
+        );
+        break;
+      }
+      case false: {
+        return (
+          <div className={`${styles.connectionHeader} ${styles.disconnected}`}>
+            <p>Disconnected.</p>
+          </div>
+        );
+        break;
+      }
+    }
+  };
+
   return (
     <main className={styles.main}>
+      {renderConnectionHeader()}
       {!gameContext.live ? <Welcome /> : <Game state={gameContext.state!} />}
     </main>
   );
