51341f7214fd5f99089bdecd7ac4c76f634c3199
Alex Eden <alexandereden91@gmail.com>
2023-09-23 17:28:14 -0500

    Send Join event on connect

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,41 @@
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import { GameEvent } from './events';
+import { GameMessage } from './messages';
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  cors: {
-    origin: '*',
-  },
-});
+const io = new Server<
+  { message: (msg: GameMessage) => void },
+  { message: (e: GameEvent, ack?: (e: number) => void) => void }
+>(httpServer, { cors: { origin: '*' } });
 
-io.on('connection', sock => {
-  console.log('connection event', sock.id);
+io.on('connection', socket => {
+  console.log('connection event', socket.id);
+  socket.onAny((event, ...args) => {
+    console.log(`got event ${event}`);
+  });
+  socket
+    .on('error', err => console.error('socket error', err))
+    .on('disconnect', reason =>
+      console.warn(`${socket.id} disconnected`, reason)
+    )
+    .on('disconnecting', reason =>
+      console.warn(`${socket.id} is disconnecting`, reason)
+    )
+    .on('message', msg => console.log('client sent a message!', msg));
+
+  setTimeout(
+    () =>
+      socket.send({
+        id: `${Date.now()}`,
+        data: { playerId: socket.id },
+        ends: null,
+        type: 'joining',
+      }),
+    500
+  );
 });
 
 httpServer.listen(3001, () => {
