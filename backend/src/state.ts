2c5775e823b9b4c91df448c29a414362059f61b1
Alex Eden <alexandereden91@gmail.com>
2023-09-23 14:57:46 -0500

    CORS update

---
+++ backend/src/state.ts
@@ -0,0 +1,46 @@
+export interface Game {
+  id: string;
+  players: Player[];
+  rounds: Round[];
+}
+
+export interface Player extends Persona {
+  id: string;
+  name: string;
+  socketId: string;
+  status: 'active' | 'eliminated';
+}
+
+export interface Persona {
+  age: number;
+  bio: string;
+  location: string;
+  name: string;
+}
+
+export type Round = {
+  id: 0 | 1 | 2 | 3;
+  currentPhase: RoundPhase;
+  previousPhases: RoundPhase[];
+  status: 'ended' | 'ongoing';
+};
+
+export type RoundPhase =
+  | {
+      type: 'chat';
+      messages: Message[];
+    }
+  | {
+      type: 'vote';
+    };
+
+export interface Message {
+  contents: string;
+  /** player ID */
+  from: string;
+  /** Date string */
+  sentAt: number;
+  /** player ID */
+  to: string;
+  type: 'answer' | 'question';
+}
